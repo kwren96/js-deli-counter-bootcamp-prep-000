2b7ae54a848b8204b59983baa74bbe517867d3b0
Katherine Wren <kwren@outlook.com>
2019-03-27 15:20:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function takeANumber(string,name) {string.push(name); return 'Welcome, ' + string.slice(-1) + '. You are number ' + ( string.length) + ' in line.'}
-function nowServing(katzDeliLine) {if (katzDeliLine.length >0) {var hold = katzDeliLine[0]; katzDeliLine.shift(); return 'Currently serving ' + hold + ".";} else {return 'There is nobody waiting to be served!'} }
-function currentLine(line) {if (line.length >0) {var i = 0; while (i<line.length); return 'The line is currently:' + i + ". " + line;} else {return 'The line is currently empty.'}}  
+function takeANumber(string,name) {
+  string.push(name);
+  return 'Welcome, ' + string.slice(-1) + '. You are number ' + ( string.length) + ' in line.'
+  
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length >0) {
+    return 'Currently serving ' +  katzDeliLine.shift() + ".";
+  } else {
+    return 'There is nobody waiting to be served!'
+  } 
+  
+}
+function currentLine(line) {
+  if (line.length >0) {
+    var xy = []
+   for(var i = 0; i < line.length; i++){
+    xy.push(`${i+1}. ${line[i]}`)
+   }
+   return `The line is currently: ${xy.join(', ')}`;
+   } else {return 'The line is currently empty.'}
+  
+}  
+
+function 
